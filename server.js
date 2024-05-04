261b3627d5245dc68fef927366379dd8cef562ac
VipCode786 <v07860@gmail.com>
2024-05-04 18:24:10 +0530

    master

--- server.js
+++ server.js
@@ -23,57 +23,8 @@ app.get("/test", (req, res) => {
   res.send("Hello world");
 });
 
-// const mergeChunks = async (fileName, totalChunks) => {
-//   const chunkDir = __dirname + "/chunks";
-//   const mergedFilePath = __dirname + "/merged_files";
 
-//   if (!fs.existsSync(mergedFilePath)) {
-//     fs.mkdirSync(mergedFilePath);
-//   }
 
-//   const writeStream = fs.createWriteStream(`${mergedFilePath}/${fileName}`);
-//   for (let i = 0; i < totalChunks; i++) {
-//     const chunkFilePath = `${chunkDir}/${fileName}.part_${i}`;
-//     const chunkBuffer = await fs.promises.readFile(chunkFilePath);
-//     writeStream.write(chunkBuffer);
-//     // fs.unlinkSync(chunkFilePath); // Delete the individual chunk file after merging
-//   }
-
-//   writeStream.end();
-//   console.log("Chunks merged successfully");
-// };
-
-// app.post("/upload", upload.single("file"), async (req, res) => {
-//   console.log("Hit");
-//   const chunk = req.file.buffer;
-//   const chunkNumber = Number(req.body.chunkNumber); // Sent from the client
-//   const totalChunks = Number(req.body.totalChunks); // Sent from the client
-//   const fileName = req.body.originalname;
-
-//   const chunkDir = __dirname + "/chunks"; // Directory to save chunks
-
-//   if (!fs.existsSync(chunkDir)) {
-//     fs.mkdirSync(chunkDir);
-//   }
-
-//   const chunkFilePath = `${chunkDir}/${fileName}.part_${chunkNumber}`;
-
-//   try {
-//     await fs.promises.writeFile(chunkFilePath, chunk);
-//     console.log(`Chunk ${chunkNumber}/${totalChunks} saved`);
-
-//     if (chunkNumber === totalChunks - 1) {
-//       // If this is the last chunk, merge all chunks into a single file
-//       await mergeChunks(fileName, totalChunks);
-//       console.log("File merged successfully");
-//     }
-
-//     res.status(200).json({ message: "Chunk uploaded successfully" });
-//   } catch (error) {
-//     console.error("Error saving chunk:", error);
-//     res.status(500).json({ error: "Error saving chunk" });
-//   }
-// });
 
 const processCSV = async (fileName, pounds,res) => {
     const filePath = __dirname + "/merged_files/" + fileName;
@@ -102,10 +53,7 @@ const processCSV = async (fileName, pounds,res) => {
       } else {
         res.status(200).json({ message: "Cheapest sellers not available for the given pounds" });
       }
-    //   res.status(200).json(cheapestSellers);
-        // Process the CSV data here
-        // For simplicity, let's just send back the entire data for now
-        // res.status(200).json(results);
+    
       });
 
  // Clean up: Delete the uploaded file
@@ -134,7 +82,6 @@ const processCSV = async (fileName, pounds,res) => {
 
   writeStream.end();
   console.log("Chunks merged successfully");
-  // await  processCSV(fileName, pounds,res);
 };
 
 
